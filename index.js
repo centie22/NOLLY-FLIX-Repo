158cf0526d4843bee4cfe1b4112650b4a610e429
centie22 <98043071+centie22@users.noreply.github.com>
2022-06-16 04:10:09 +0100

    NollyFlix

--- index.js
+++ index.js
@@ -1,15 +1,81 @@
 console.log('hello there');
 
 
-let likesEl = document.querySelector('.likes-number');
+
+// likesEl.textContent = 56;
+// let count = 0;
+// function like() {
+//     count+= 1
+//         console.log(count)
+//     likesEl.innerText = count
+    
+//     console.log('movie has been liked')
+//     likeIcon.forEach(like())
+// }
+
+
+
+// let likesEl = document.querySelectorAll('.likes-number').forEach(likesEl => {
+//     likesEl.addEventListener('click', like(){
+//         
+//     })
+// })
+
+// likeIcon.forEach(like-icon == {
+// like-icon.addEventListener('click' , function  like(){
+//     let count = 0
+//     count += 1
+//     console.log('button clicked', like());
+
+// })
+// })
+
+let likesEl = document.querySelectorAll('.likes-number');
+let likeIcon = document.querySelectorAll('.likeicon');
 let priceTag = document.querySelector('#price-tag');
 
-//likesEl.textContent = 56;
-let count = 0;
-function like() {
-    count+= 1
-    console.log(count)
-    likesEl.innerText = count
-    
-    console.log('movie has been liked')
+
+const movieData = {
+    movieList : [
+        {
+            id : 0,
+            title : "The Redemption"
+        },
+            {
+                id : 1,
+                title : "Africa's Tech Roots"
+            },
+            {
+                id : 2,
+                title : "The Invisible Man"
+            },
+            {
+                id : 3,
+                title : "The Complicated Project"
+            },
+            {
+                id : 4,
+                title : "Simply Javascript"
+            },
+            {
+                id : 5,
+                title : "The New Web"
+            }
+    ]
 }
+let count = 0
+likeIcon.forEach (likeicon => {
+        likeicon.addEventListener('click' , function like(){
+        likesEl.values = parseInt(likesEl.values) + 1;
+        
+        console.log('button clicked')
+    })
+    })
+
+    function like(){
+        let count = 0
+        count += 1
+        // count +-1
+        // let likeEl = likesEl.innerHTML = count
+    }
+    
